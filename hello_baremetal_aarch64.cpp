06b4bbbaa0d86ac5268f3158dd8f962d1d9146f2
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2024-12-05 14:11:58 +0000

    adding consteval examples
    Signed-off-by: Stefano Fiorentino <stefano.fiorentino@adesso.ch>

--- hello_baremetal_aarch64.cpp
+++ hello_baremetal_aarch64.cpp
@@ -6,28 +6,73 @@ struct array
 {
     consteval explicit array() {
         data = new T[_Size];
-        data[0] = 42;
+        for (int i=0; i<_Size; i++) {
+            data[i] = 0;
+        }
+        index = 0;
     }
-    consteval T front() const { return data[0]; }
-    consteval T back() const {return data[_Size-1];}
-    consteval unsigned int size() const { return _Size; }
-    consteval T& operator[] (unsigned int index) {
-        return *(data+index);
+    consteval T const& front() const { return data[0]; }
+    consteval T const& back() const { return data[(index)?index-1:0]; }
+    consteval unsigned int size() const { return index; }
+    consteval unsigned int capacity() const { return _Size; }
+    consteval T& operator[] (unsigned int _index) {
+        return *(data+_index);
     }
-    constexpr ~array() {delete[] data;}
+    consteval T const& push_back(T t) {
+        data[index++]=t;
+        return back();
+    }
+    consteval T const& pop_back() {
+        return data[--index];
+    }
+    constexpr ~array() noexcept{delete[] data;}
 private:
     T* data;
+    unsigned int index;
 };
 }
 
 consteval void test_size() {
-    static_assert(array<int, ARRAY_SIZE>().size() == ARRAY_SIZE);
+    static_assert(0 == array<int, ARRAY_SIZE>().size());
 }
+
+consteval void test_capacity() {
+    static_assert(ARRAY_SIZE == array<int, ARRAY_SIZE>().capacity());
+}
+
 consteval void test_subscript() {
-    static_assert(array<int, ARRAY_SIZE>()[0] ==  42);
+    static_assert(0 == array<int, ARRAY_SIZE>()[0]);
+}
+
+consteval void test_front() {
+    static_assert(0 == array<int, ARRAY_SIZE>().front());
+}
+
+consteval void test_back() {
+    static_assert(0 == array<int, ARRAY_SIZE>().back());
+}
+
+consteval unsigned int size_after_one_push_back() {
+    auto arr = array<int, ARRAY_SIZE>();
+    arr.push_back(84);
+    return arr.size();
+}
+
+consteval void test_push_back() {
+    static_assert(84 == array<int, ARRAY_SIZE>().push_back(84));
+    static_assert(1 == size_after_one_push_back());
+}
+
+consteval unsigned int size_after_one_push_back_and_one_pop_back() {
+    auto arr = array<int, ARRAY_SIZE>();
+    arr.push_back(84);
+    arr.pop_back();
+    return arr.size();
+}
+
+consteval void test_pop_back()  {
+    static_assert(0 == size_after_one_push_back_and_one_pop_back());
 }
 
 extern "C" void c_entry() {
-  test_size();
-  test_subscript();
 }
