951e47484c358b5e93dbda074eabca1bc400ece5
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2025-02-10 15:16:00 +0000

    step 1 finalized
    Signed-off-by: Stefano Fiorentino <stefano.fiorentino@adesso.ch>

--- step1.hpp
+++ step1.hpp
@@ -1,11 +1,21 @@
+// kas shell bare-metal.yml
+// devtool modify baremetal-helloworld
+// bitbake -c build baremetal-helloworld
+// runqemu nographic slirp
+
 /* VIRT_UART0 base address is 0x10000000 according to QEMU source code*/
 #define VIRT_UART0 0x10000000
 
-volatile unsigned int * const uart0 = (volatile unsigned int*) UART0_ADDR;
-
-constexpr void print(volatile unsigned int* const uart, char const * const str) {
+constexpr void print(volatile unsigned int* const uart, const char * str) {
     while(*str != '\0')
     {
         *uart = (unsigned int) *str;
+        str++;
     }
+}
+
+extern "C" void c_entry() {
+    volatile unsigned int * const uart0 = (volatile unsigned int*) VIRT_UART0;
+    print(uart0, "Hello, world!\n");
+    while(1);
 }
